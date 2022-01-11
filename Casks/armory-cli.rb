fc4737e07876c2251f338bf879dafe94198632a0
Todd Underwood <todd.underwood@armory.io>
2022-01-10 16:22:49 -0800

    test3

--- Casks/armory-cli.rb
+++ Casks/armory-cli.rb
@@ -1,8 +1,10 @@
 cask "armory-cli" do
-  version "v0.11.7"
-  sha256 "e4a8462ae262911c0c762578e0314f0e04d920240d56dbe5fef67972ff031cfd"
+  version "0.11.7"
+  sha256 :no_check
 
-  url "https://github.com/armory-io/armory-cli/releases/download/v0.11.7/armory-darwin-amd64"
+  url "https://github.com/armory-io/armory-cli/releases/download/v#{version}/armory-darwin-amd64", 
+    verified: "github.com/armory-io/"
+  appcast "https://github.com/armory-io/armory-cli/releases.atom"
   name "Armory CLI"
   desc "CLI for Armory Cloud"
   homepage "https://www.armory.io/"
